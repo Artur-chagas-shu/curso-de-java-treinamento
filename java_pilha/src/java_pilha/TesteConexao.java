82bf0ecc2ce10ed05967bd3873a099c5d578a4cf
Artur-chagas-shu <artur_life_2015@hotmail.com>
2023-04-03 01:03:55 -0300

    Adicionandos classes , Conexao e TestaConexao
    classes aonde se tem um exemplo simples de conexão e uma com o teste dela.

---
+++ java_pilha/src/java_pilha/TesteConexao.java
@@ -0,0 +1,19 @@
+package java_pilha;
+
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		Conexao con = null;
+		try {
+			con = new Conexao();
+			con.leDados();
+
+		} catch (Exception ex) {
+			System.out.println("Deu erro na conexao");
+
+		} finally {
+			con.fecha();
+		}
+	}
+
+}
