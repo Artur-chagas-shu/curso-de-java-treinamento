f6e4839e4e1d0829a20d2be0cb4d6faa3126adcd
Artur-chagas-shu <artur_life_2015@hotmail.com>
2023-04-03 01:43:09 -0300

    Adicionando Try with resources e implements Autoclousable a conexao

--- java_pilha/src/java_pilha/TesteConexao.java
+++ java_pilha/src/java_pilha/TesteConexao.java
@@ -3,17 +3,41 @@ package java_pilha;
 public class TesteConexao {
 
 	public static void main(String[] args) {
-		Conexao con = null;
-		try {
-			con = new Conexao();
-			con.leDados();
-
-		} catch (Exception ex) {
+		
+		
+		try (Conexao conexao = new Conexao()){
+			conexao.leDados();	
+		} catch(Exception ex) {
 			System.out.println("Deu erro na conexao");
-
-		} finally {
-			con.fecha();
 		}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		// ---------------------------------------------------
+//
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//
+//		} catch (Exception ex) {
+//			System.out.println("Deu erro na conexao");
+//
+//		} finally {
+//			if (con != null) {
+//				con.close();
+//			}
+//		}
 	}
 
-}
+
